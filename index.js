9c888ffeb80b934e20258b619928907db2cfdea4
slikts <dabas@untu.ms>
2017-04-10 13:26:23 +0300

    Params

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const moment = require('moment')
 
-const makeGet = name => date => require(`./data/${name}.json`)[date]
+const makeGet = name => date => require(`./data/${name}.json`)[date.format('DD-MM')]
 const getVd = makeGet('vd')
 const getVdExt = makeGet('vd-ext')
 const get = date => getVd(date).concat(getVdExt(date))
-const today = moment().format('DD-MM')
+const parseDate = text => new Date(text).toJSON()
+const arg = process.argv[2]
+const argDate = parseDate(arg)
+const date = argDate ? moment(argDate) : moment()
 const format = arr => arr.join(', ')
 const { log } = console
-const frmToday = moment().locale('lv').format('Do MMMM')
-const vds = format(getVd(today))
-const vdsExt = format(getVdExt(today))
+const frmDate = date.locale('lv').format('Do MMMM')
+const vds = format(getVd(date))
+const vdsExt = format(getVdExt(date))
 
-log(`🎉 ${frmToday}:  ${vds}`)
+log(`🎉 ${frmDate}:  ${vds}`)
 log(`🤷️ ${vdsExt}`)
