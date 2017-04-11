bc13c0891d50403a52230dbcbe21ffd222ea9ac2
slikts <dabas@Untu.ms>
2017-04-12 02:12:32 +0300

    Release 2.0.6

---
+++ src/data.js
@@ -0,0 +1,15 @@
+const { Map, List } = require('immutable')
+const { namedays, extNamedays } = require('vardadienas')
+
+const vd = new Map(namedays)
+const ext = new Map(extNamedays)
+const all = () =>
+  vd
+  .map((names, dateKey) => names.concat(ext.get(dateKey)))
+  .map((names, dateKey) => new List(names.map(name => ({ name, dateKey }))))
+  .toList()
+  .flatten()
+  .toArray()
+const data = { vd, ext, all }
+
+module.exports = data
