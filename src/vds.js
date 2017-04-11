f38e11903ae6300f24995f73953410207f0c3a2e
slikts <dabas@Untu.ms>
2017-04-11 19:38:25 +0300

    Release 2.0.3

--- src/vds.js
+++ src/vds.js
@@ -3,14 +3,14 @@ const { Map, List } = require('immutable')
 const Fuse = require('fuse.js')
 
 const { log } = console
-const formatNames = arr => `**${arr.join(', ')}**`
-const formatNamesAlt = arr => `${arr.join(', ')}`
+const formatNames = names => `**${names.join(', ')}**`
+const formatNamesAlt = names => `${names.join(', ')}`
 const localeFormatDate = date => date.locale('lv').format('Do MMMM')
 const dateFromKey = isoMonDay => moment(`0000-${isoMonDay}`)
-const getArg = (args = process.argv) => args.includes('--') ? args.slice(args.lastIndexOf('--') + 1).join(' ') : null
+const getArg = (args = process.argv) => (args.includes('--') ? args.slice(args.lastIndexOf('--') + 1).join(' ') : null)
 
 const data = (() => {
-  const getItem = (name) => new Map(require(`../data/${name}.json`))
+  const getItem = name => new Map(require(`../data/${name}.json`))
   const vd = getItem('vd')
   const ext = getItem('vd-ext')
   return {
@@ -23,11 +23,11 @@ const data = (() => {
       .toList()
       .flatten()
       .toArray()
-    }
+    },
   }
 })()
 
-const getVds = date => {
+const getVds = (date) => {
   const key = date.format('MM-DD')
   return {
     vd: data.vd.get(key),
@@ -42,17 +42,18 @@ const renderDate = (dateSource = new Date()) => {
   log(`🤓 ${formatNamesAlt(vds.ext)}`)
 }
 
-const searchNames = text => {
+const searchNames = (text) => {
   const f = new Fuse(data.all(), {
     keys: ['name'],
     threshold: 0.2,
   })
-  const collect = (results, {name, dateKey}) => results.set(dateKey, (results.get(dateKey) || []).concat(name))
+  const collect = (results, { name, dateKey }) =>
+    results.set(dateKey, (results.get(dateKey) || []).concat(name))
   return f.search(text)
   .reduce(collect, new Map())
 }
 
-const renderSearch = results => {
+const renderSearch = (results) => {
   results.forEach((names, key) => {
     const date = localeFormatDate(dateFromKey(key))
     log(`${date}: ${formatNames(names)}`)
@@ -61,20 +62,20 @@ const renderSearch = results => {
 
 const run = (arg = getArg(process.argv)) => {
   const dateArg = new Date(arg).toJSON()
-  if (arg) {
-    if (dateArg) {
-      renderDate(dateArg)
-      return
-    }
-    const results = searchNames(arg)
-    if (results.size) {
-      renderSearch(results)
-      return
-    }
-    log('?¿?¿?')
+  if (!arg) {
+    renderDate()
     return
   }
-  renderDate()
+  if (dateArg) {
+    renderDate(dateArg)
+    return
+  }
+  const results = searchNames(arg)
+  if (results.size) {
+    renderSearch(results)
+    return
+  }
+  log('?¿?¿?')
 }
 
 module.exports = run
