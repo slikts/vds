8feeba91365cd06302c4759445ffa2da4f7c1da1
slikts <dabas@Untu.ms>
2017-04-12 17:12:50 +0300

    Release 2.0.15

--- src/data.js
+++ src/data.js
@@ -1,11 +1,12 @@
 const { Map, List } = require('immutable')
 const { namedays, extNamedays } = require('vardadienas')
 
+const formatExt = name => `_${name}_`
 const vd = new Map(namedays)
 const ext = new Map(extNamedays)
 const all = () =>
   vd
-  .map((names, dateKey) => names.concat((ext.get(dateKey) || []).map(name => `_${name}_`)))
+  .map((names, dateKey) => names.concat((ext.get(dateKey) || []).map(formatExt)))
   .map((names, dateKey) => new List(names.map(name => ({ name, dateKey }))))
   .toList()
   .flatten()
